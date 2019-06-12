7844551c40284b341bb7a27c3a41f67394ee1db8
John Cole (Laptop) <homeserverstatus@gmail.com>
2019-06-12 22:32:58 +0100

    update testing log

--- codebase/fileprocessing_component/fileprocessing_module.py
+++ codebase/fileprocessing_component/fileprocessing_module.py
@@ -115,7 +115,7 @@ def buildpath(nodelist):
 def getloggingdata(loggingmode):
 	Logging.printout("Loading Logs")
 	loggingoutput = []
-	for filename in ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9']:
+	for filename in ['9', '8', '7', '6', '5', '4', '3', '2', '1', '0']:
 		logcontents = FileSystem.readfromdisk('./data/logging-' + filename + '.log')
 		print('---', filename, '---', logcontents)
 		if (filename != '0') and (logcontents != ""):
