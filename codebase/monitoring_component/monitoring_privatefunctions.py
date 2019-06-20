42669744f42f3418389bac952e703d39894ce5e0
John Cole (Laptop) <homeserverstatus@gmail.com>
2019-06-20 23:26:31 +0100

    markers are 3 hours

--- codebase/monitoring_component/monitoring_privatefunctions.py
+++ codebase/monitoring_component/monitoring_privatefunctions.py
@@ -7,12 +7,13 @@ def getxaxis(nowtimedate, erasize, boxwidth, horizontaloffset):
 	markersoutcome = []
 	labelsoutcome = []
 	currentmarker = EraFunctions.geteraasobject(nowtimedate, 5)
-	currentmarker.adjusthours(-3)
+	while ((int(currentmarker[8:10])) % 3) != 0:
+		currentmarker.adjusthours(-1)
 	markerposition = 0
 	while markerposition < 960:
 		currentmarker.adjusthours(3)
 		markerposition = ((boxwidth + 1) * EraFunctions.geteradifference(nowtimedate, currentmarker,
-																						erasize)) + horizontaloffset
+																					erasize)) + horizontaloffset + 1
 		if markerposition >= horizontaloffset:
 			instruction = 'x1="' + str(markerposition) + '" y1="130" x2="' + str(markerposition) + '" y2="132"'
 			markersoutcome.append(instruction)
