5dc56761e1909ca880ef3c9f70b4c76fbb90ca03
John Cole (Laptop) <homeserverstatus@gmail.com>
2019-06-20 23:46:31 +0100

    smarter markers

--- codebase/monitoring_component/monitoring_privatefunctions.py
+++ codebase/monitoring_component/monitoring_privatefunctions.py
@@ -7,16 +7,16 @@ def getxaxis(nowtimedate, erasize, boxwidth, horizontaloffset):
 	markersoutcome = []
 	labelsoutcome = []
 	currentmarker = EraFunctions.geteraasobject(nowtimedate, 5)
-	while (currentmarker.gettimevalue() % 10800) != 0:
-		currentmarker.adjusthours(-1)
+	currentmarker.adjusthours(-1)
 	markerposition = 0
 	while markerposition < 960:
-		currentmarker.adjusthours(3)
+		currentmarker.adjusthours(1)
 		markerposition = ((boxwidth + 1) * EraFunctions.geteradifference(nowtimedate, currentmarker,
 																					erasize)) + horizontaloffset + 1
 		if markerposition >= horizontaloffset:
 			instruction = 'x1="' + str(markerposition) + '" y1="130" x2="' + str(markerposition) + '" y2="132"'
 			markersoutcome.append(instruction)
-			instruction = 'x="' + str(markerposition) + '" y="145" >' + EraFunctions.geteralabel(currentmarker, erasize)
-			labelsoutcome.append(instruction)
+			if (currentmarker.gettimevalue() % 10800) == 0:
+				instruction = 'x="' + str(markerposition) + '" y="145" >' + EraFunctions.geteralabel(currentmarker, erasize)
+				labelsoutcome.append(instruction)
 	return {"markers": markersoutcome, "labels": labelsoutcome}
