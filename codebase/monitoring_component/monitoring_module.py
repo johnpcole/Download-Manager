6093d54550293fc2a220bfedde42d3b708c64f5c
John Cole (Laptop) <homeserverstatus@gmail.com>
2019-06-21 20:35:49 +0100

    fix 2

--- codebase/monitoring_component/monitoring_module.py
+++ codebase/monitoring_component/monitoring_module.py
@@ -12,7 +12,7 @@ def getloadlist():
 	timedate = DateTime.getnow()
 	timedate.adjustdays(-3)
 	outcome = []
-	for index in range[0, 3]:
+	for index in range(0, 3):
 		timedate.adjustdays(1)
 		outcome.append(timedate.getdateiso())
 
