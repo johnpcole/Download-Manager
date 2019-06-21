39b44f05cd937bc39e8d3cf6364a7ea0bc8b7c47
John Cole (Laptop) <homeserverstatus@gmail.com>
2019-06-21 21:38:31 +0100

    align bars better

--- codebase/monitoring_component/historyitem_subcomponent/historyitem_class.py
+++ codebase/monitoring_component/historyitem_subcomponent/historyitem_class.py
@@ -77,14 +77,14 @@ class DefineItem:
 	def getuploadgraphicdata(self, horizontaloffset, verticaloffset, boxwidth, baselineuploaded, origintimeobject, erasize):
 
 		outcome = []
-		vert = verticaloffset + 260
 		horizontalinstruction = EraFunctions.geteradifference(origintimeobject, self.datetime, erasize)
 		if horizontalinstruction > -1:
 			boxheight = (125 * (self.uploaded - baselineuploaded)) / 1000000000
 			print(boxheight)
 			if boxheight > -1:
+				ver = verticaloffset + 260 - boxheight
 				hor = horizontaloffset + (horizontalinstruction * (boxwidth + 1))
-				instruction = 'x="' + str(hor) + '" y="' + str(vert)
+				instruction = 'x="' + str(hor) + '" y="' + str(ver)
 				instruction = instruction + '" width="' + str(boxwidth) + '" height="' + str(boxheight) + '"'
 				outcome.append(instruction)
 
