1f89e708653b6d8fe2bdf831e4d528a5ebea7dd8
John Cole (Laptop) <homeserverstatus@gmail.com>
2019-06-22 14:18:23 +0100

    vpn checking

--- codebase/monitoring_component/network_subcomponent/network_module.py
+++ codebase/monitoring_component/network_subcomponent/network_module.py
@@ -4,12 +4,17 @@ import os as OperatingSystem
 def getvpnstatus():
 
 	result = OperatingSystem.popen('ifconfig').readline()
+	print("<><><><><><><><><><><><><><><><><><><><><><><><>")
+	print(result)
+	print("<><><><><><><><><><><><><><><><><><><><><><><><>")
 	outcome = 0
 	for logrow in result:
+		print(logrow)
 		if logrow.find("<UP,") > 0:
 			splitdata = logrow.split(": flags=")
 			if splitdata[0] == "tun0":
 				outcome = 1
+				print("Found it")
 	print("checked vpn status: ", outcome)
 	return outcome
 
