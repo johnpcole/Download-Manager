f06773d8f6193d63fcddf8661fb591475f2f8c90
John Cole (Laptop) <homeserverstatus@gmail.com>
2019-06-22 15:36:33 +0100

    remove prting

--- codebase/monitoring_component/network_subcomponent/network_module.py
+++ codebase/monitoring_component/network_subcomponent/network_module.py
@@ -6,7 +6,6 @@ def getvpnstatus():
 	result = OperatingSystem.popen('ifconfig')
 	outcome = 0
 	for logrow in result.readlines():
-		print(logrow)
 		if logrow.find("<UP,") > 0:
 			splitdata = logrow.split(": flags=")
 			if splitdata[0] == "tun0":
