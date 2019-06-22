d9e3953ad305967013cf6293f111174f2d3c9b26
John Cole (Laptop) <homeserverstatus@gmail.com>
2019-06-22 14:42:15 +0100

    read ifconfig output properly

--- codebase/monitoring_component/network_subcomponent/network_module.py
+++ codebase/monitoring_component/network_subcomponent/network_module.py
@@ -3,12 +3,12 @@ import os as OperatingSystem
 
 def getvpnstatus():
 
-	result = OperatingSystem.popen('ifconfig').readline()
+	result = OperatingSystem.popen('ifconfig')
 	print("<><><><><><><><><><><><><><><><><><><><><><><><>")
 	print(result)
 	print("<><><><><><><><><><><><><><><><><><><><><><><><>")
 	outcome = 0
-	for logrow in result:
+	for logrow in result.readlines():
 		print(logrow)
 		if logrow.find("<UP,") > 0:
 			splitdata = logrow.split(": flags=")
