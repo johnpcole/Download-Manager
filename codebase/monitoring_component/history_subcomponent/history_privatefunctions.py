7e85831680abd22e5c29eee9d3d4cd9d08ea41da
John Cole (Laptop) <homeserverstatus@gmail.com>
2019-06-23 23:32:26 +0100

    move again

--- codebase/monitoring_component/history_subcomponent/history_privatefunctions.py
+++ codebase/monitoring_component/history_subcomponent/history_privatefunctions.py
@@ -68,9 +68,9 @@ def getgraphblocks(origintimedate, erasize, boxwidth, horizontaloffset, firsttop
 							outcome[colourkey[2:]].append(instruction)
 
 			if historyitem.getuploaded() > 0:
-				barheight = calculatebarheight(graphheight - 5, historyitem.getuploaded())
-				barheight = calculatebarheight(graphheight - 5, 100000000000000)
-				outcome['blue'].append(printrectangle(column, secondtop - barheight, boxwidth, barheight))
+				barheight = calculatebarheight(graphheight, historyitem.getuploaded())
+				barheight = calculatebarheight(graphheight, 100000000000000)
+				outcome['blue'].append(printrectangle(column, secondtop - barheight - 2, boxwidth, barheight))
 
 	return outcome
 
