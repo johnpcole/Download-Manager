c0b5e66cd9d4df501440914033fe469b3ce5d856
John Cole (Laptop) <homeserverstatus@gmail.com>
2019-06-23 22:53:09 +0100

    get positioning correct

--- codebase/monitoring_component/history_subcomponent/history_privatefunctions.py
+++ codebase/monitoring_component/history_subcomponent/history_privatefunctions.py
@@ -100,4 +100,4 @@ def calculatecolumnposition(boxwidth, horizontaloffset, origindatetime, bardatet
 
 def calculaterowposition(boxheight, verticaloffset, previousboxes):
 
-	return ((boxheight + 1) * previousboxes) + verticaloffset
+	return verticaloffset - ((boxheight + 1) * previousboxes) - 1
