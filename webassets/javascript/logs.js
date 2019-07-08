f4d9737e3e5af1f145f74ce47f8651c954dfdbfb
John Cole (Laptop) <homeserverstatus@gmail.com>
2019-07-08 22:33:43 +0100

    change indicator

--- webassets/javascript/logs.js
+++ webassets/javascript/logs.js
@@ -8,22 +8,37 @@ $(document).ready(function ()
 
 
 
-function showHideRows(rownumber)
+function showHideRows(rowNumber)
 {
-    for( var subrow = 0; subrow < 100; subrow++ ) {
-        var subrowfractionid = "000";
-        subrowfractionid = subrowfractionid.concat(subrow.toString());
-        subrowfractionid = subrowfractionid.substring(subrowfractionid.length - 2, 999)
-        var subrowid = rownumber.toString()
-        subrowid = subrowid.concat(".", subrowfractionid)
-        var areaobject = document.getElementById(subrowid);
-        if (areaobject != null) {
-            var currentclass = areaobject.className
-            if (currentclass.substring(0, 6) == 'hidden') {
-                areaobject.className = currentclass.substring(6, currentclass.length);
+    var masterRowName = "Entry".concat(rowNumber.toString());
+    var currentStatus = getImageName(masterRowName);
+    if (currentStatus == 'collapsed_indicator') {
+        rerenderImage(masterRowName, 'expanded_indicator');
+    } else {
+        rerenderImage(masterRowName, 'collapsed_indicator');
+    };
+    for( var subRow = 0; subRow < 100; subRow++ ) {
+        var subRowId = getSubRowId(rowNumber, subRow);
+        var areaObject = document.getElementById(subRowId);
+        if (areaObject != null) {
+            var baseClass = areaObject.className
+            var baseClass = baseClass.substring(4, currentclass.length);
+            if (currentStatus == 'collapsed_indicator') {
+                changeAreaClass(areaObject, "show".concat(baseClass));
             } else {
-                areaobject.className = "hidden".concat(areaobject.className);
+                changeAreaClass(areaObject, "hide".concat(baseClass));
             };
         };
     };
+};
+
+
+
+function getSubRowId(row, subrow)
+{
+    var subrowfractionid = "000".concat(subrow.toString());
+    subrowfractionid = subrowfractionid.substring(subrowfractionid.length - 2, 999)
+    var subrowid = rowNumber.toString()
+    subrowid = subrowid.concat(".", subrowfractionid)
+    return subrowid;
 };
