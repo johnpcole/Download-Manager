441f5793b0f663a1cb7daac43f762da21d4ad980
John Cole (Laptop) <homeserverstatus@gmail.com>
2019-08-17 22:44:18 +0100

    another test

--- codebase/operator_component/delugeinterface_subcomponent/delugeinterface_class.py
+++ codebase/operator_component/delugeinterface_subcomponent/delugeinterface_class.py
@@ -30,6 +30,7 @@ class DefineDelugeInterface:
 
 		# Get the list of torrent GUIDs from the Delude Daemon (as a flat list)
 		reportedtorrentidlist = self.delugeclient.retrievetorrentlist()
+		print("TORRENTLIST: "+reportedtorrentidlist)
 
 		# Update all the torrents' data relevent
 		if reportedtorrentidlist is not None:
@@ -60,7 +61,7 @@ class DefineDelugeInterface:
 
 		self.retrievealldelugedata()
 
-		self.delugeclient.closeconnection()
+		#self.delugeclient.closeconnection()
 
 
 
