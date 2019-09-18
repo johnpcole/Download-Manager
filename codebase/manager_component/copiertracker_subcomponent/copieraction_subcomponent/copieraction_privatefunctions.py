ecf046fa5861a20b439cedf6fd4fd5bcba001794
John Cole (Laptop) <homeserverstatus@gmail.com>
2019-09-18 16:26:26 +0100

    another table attempt

--- codebase/manager_component/copiertracker_subcomponent/copieraction_subcomponent/copieraction_privatefunctions.py
+++ codebase/manager_component/copiertracker_subcomponent/copieraction_subcomponent/copieraction_privatefunctions.py
@@ -33,57 +33,60 @@ def sanitisecopydatetimestamp(actionid):
 
 def rendercopyresults(results, statusenum, targetpath):
 
-	outcome = []
-	columncount = 0
+	titles = []
+	subtitles = []
+	sizes = []
+	datetimes = []
 
 	if "Source File" in results.keys():
-		outcome.append("Downloaded File")
-		outcome.append("(On Downloads drive)")
-		outcome.extend(rendercopyresultdetail(results["Source File"]))
-		columncount = columncount + 1
+		titles.append("Downloaded File")
+		subtitles.append("(On Downloads drive)")
+		sizes.append(rendercopysize(results["Source File"]))
+		datetimes.append(rendercopydatetime(results["Source File"]))
 
 	if "Existing File" in results.keys():
 		if statusenum.get("Succeeded") == True:
-			outcome.append("Pre-existing File which was overwritten")
+			titles.append("Pre-existing File which was overwritten")
 		elif statusenum.get("Confirm") == True:
-			outcome.append("Pre-existing File which will be overwritten if confirmed")
+			titles.append("Pre-existing File which will be overwritten if confirmed")
 		else:
-			outcome.append("Pre-existing File which would have been overwritten")
-		outcome.append("(On Videos drive)")
-		outcome.extend(rendercopyresultdetail(results["Existing File"]))
-		columncount = columncount + 1
+			titles.append("Pre-existing File which would have been overwritten")
+		subtitles.append("(On Videos drive)")
+		sizes.append(rendercopysize(results["Existing File"]))
+		datetimes.append(rendercopydatetime(results["Existing File"]))
 
 	if "New Copied File" in results.keys():
-		outcome.append("Downloaded File")
-		outcome.append("(On Downloads drive)")
-		outcome.extend(rendercopyresultdetail(results["New Copied File"]))
-		columncount = columncount + 1
+		titles.append("Downloaded File")
+		subtitles.append("(On Downloads drive)")
+		sizes.append(rendercopysize(results["New Copied File"]))
+		datetimes.append(rendercopydatetime(results["New Copied File"]))
 
 	if "Error" in results.keys():
-		outcome.append("File not copied!")
-		outcome.append("(Error encountered)")
-		outcome.extend(results["Error"])
-		outcome.extend("")
-		columncount = columncount + 1
+		titles.append("File not copied!")
+		subtitles.append("(Error encountered)")
+		sizes.extend(results["Error"])
+		datetimes.extend("")
 
-	realoutcome = {"filepath": sanitisetargetpath(targetpath)}
-
-	newoutcome = []
-	for rowval in range(0, 4):
-		for colval in range(0, columncount+1):
-			newoutcome.append(outcome[(rowval * (columncount+1)) + colval])
-	realoutcome["outcomes"] = newoutcome
+	realoutcome = {"filepath": sanitisetargetpath(targetpath), "outcomes": [titles, subtitles, sizes, datetimes]}
 
 	return realoutcome
 
 
-def rendercopyresultdetail(filedetails):
+def rendercopysize(filedetails):
 
-	outcome = []
-	outcome.append(DataConversion.sanitisesize(filedetails["filesize"]))
+	return DataConversion.sanitisesize(filedetails["filesize"])
+
+
+def rendercopydatetime(filedetails):
+
 	datetime = sanitisecopydatetimestamp(filedetails["datetime"] + "000")
-	outcome.append(datetime[:-3])
-	return outcome
+	return datetime[:-3]
+
+
+
+
+
+
 
 
 
