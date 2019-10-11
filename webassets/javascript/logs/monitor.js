48eff5ca47c32bed7faaa3c4e7ddbc598603ca08
John Cole (Laptop) <homeserverstatus@gmail.com>
2019-10-11 20:19:00 +0100

    single button to switch views

---
+++ webassets/javascript/logs/monitor.js
@@ -0,0 +1,28 @@
+//Sets up the page load
+
+$(document).ready(function ()
+{
+    $('#ajaxloader').hide();
+});
+
+function switchView()
+{
+    var newurl = "/Monitor=" + getNewView
+    window.location.replace(newurl);
+};
+
+
+function getNewView()
+{
+    var pathname = window.location.pathname;
+    var currentview = pathname.substring(9);
+    if (currentview == 'Recent') {
+        var newview = 'Latest';
+    } else {
+        var newview = 'Recent';
+    };
+    return newview;
+};
+
+
+
