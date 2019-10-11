c6f6c753e0f12e44ca30682366dfcde554ea2518
John Cole (Laptop) <homeserverstatus@gmail.com>
2019-10-11 18:56:20 +0100

    test15

--- codebase/manager_component/monitoring_subcomponent/history_subcomponent/graphing_subcomponent/graph_subcomponent/graph_module.py
+++ codebase/manager_component/monitoring_subcomponent/history_subcomponent/graphing_subcomponent/graph_subcomponent/graph_module.py
@@ -121,7 +121,7 @@ def creategraphaxes(origintimedate, erasize, boxwidth, horizontaloffset, graphbo
 
 	outcome = createblank()
 
-	colmax = graphwidth - (horizontaloffset + 4)
+	colmax = graphwidth - (horizontaloffset + 6)
 	colmin = horizontaloffset + 2
 
 	#horizontal axes
