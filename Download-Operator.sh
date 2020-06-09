df535363f7e4d30d4ec67648017cb1c816ec0cad
John Cole (Laptop) <homeserverstatus@gmail.com>
2020-06-09 01:38:08 +0100

    debug 42

--- Download-Operator.sh
+++ Download-Operator.sh
@@ -2,6 +2,6 @@
 
 cd /mnt/Torrents-Disk/Download-Manager
 
-python3 operator_application.py >/mnt/Torrents-Disk/Download-Manager/data/application_logs/monitor.log 2>&1
+python3 operator_application.py >/mnt/Torrents-Disk/Download-Manager/data/application_logs/operator.log 2>&1
 
 
